228167ae3c73da8e95356da282a2d13a0dbcaa05
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-05-22 16:18:01 +0200

    feat: add AppDbContext with DbSet<TaskItem>

---
+++ src/Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+}
