d969fb6e0c35dd0ee85bbf615e52f8e9b7c8b745
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-05-22 16:18:43 +0200

    feat: add DTOs for task create and update requests

---
+++ src/Api/Dtos/Requests/TaskCreateRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Api.Dtos;
+
+public class TaskCreateRequest
+{
+    public string Title { get; set; } = null!;
+    public DateTime? DueDate { get; set; }
+}
---
+++ src/Api/Dtos/Requests/TaskUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Dtos;
+
+public class TaskUpdateRequest
+{
+    public string Title { get; set; } = null!;
+    public bool IsDone { get; set; }
+    public DateTime? DueDate { get; set; }
+}
+
