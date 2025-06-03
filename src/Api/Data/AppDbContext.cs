a9dd39cf6a07bcbf2f89ea8dc298a98f9a348cda
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-06-03 16:54:04 +0200

    feat(data): update AppDbContext to support Identity and User-Task relationship

--- src/Api/Data/AppDbContext.cs
+++ src/Api/Data/AppDbContext.cs
@@ -1,14 +1,18 @@
 ﻿using Api.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Api.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<User>
+
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {
     }
 
-    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+    public DbSet<TaskItem>Tasks { get; set; }
+    public DbSet<User> Users { get; set; }
 }
