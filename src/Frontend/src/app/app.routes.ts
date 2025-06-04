87bae8e38af5997e4c8f13e8a4d5ea4c87a14f0f
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-06-04 12:56:16 +0200

    feat(router): add routes for login and register pages

--- src/Frontend/src/app/app.routes.ts
+++ src/Frontend/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import {TaskListComponent} from './tasks/task-list/task-list.component';
 import {TaskFormComponent} from './tasks/task-form/task-form.component';
+import {LoginComponent} from './_auth/auth/login/login.component';
+import {RegisterComponent} from './_auth/auth/register/register.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/tasks', pathMatch: 'full' },
   { path: 'tasks', component: TaskListComponent },
-  { path: 'tasks/new', component: TaskFormComponent }
+  { path: 'tasks/new', component: TaskFormComponent },
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent }
 ];
