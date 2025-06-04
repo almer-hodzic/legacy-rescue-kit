93a5b1bb5b53bed48150fecf4bd99218246e091c
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-06-04 12:56:33 +0200

    chore(core): register auth interceptor in application config

--- src/Frontend/src/app/app.config.ts
+++ src/Frontend/src/app/app.config.ts
@@ -3,11 +3,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
 import { provideZoneChangeDetection } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import {AuthInterceptor} from './_auth/auth/auth.interceptor';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
-    provideZoneChangeDetection({ eventCoalescing: true })
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ]
 };
