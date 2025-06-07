3f7d1643308c3aa88ac97cb99105dac8139f21cd
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-06-07 09:40:30 +0200

    feat(navbar): add user dropdown with username, logout and profile options

--- src/Frontend/src/app/_layout/navbar/navbar.component.ts
+++ src/Frontend/src/app/_layout/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {AuthService} from '../../_auth/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +11,25 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(public auth: AuthService, private router: Router) {}
+
+  logout(): void {
+    this.auth.logout();
+    this.router.navigate(['/auth/login']);
+  }
+
+  get username(): string | null {
+    return this.auth.getUsername();
+  }
+
+  showDropdown = false;
+
+
+  goToProfile(): void {
+    this.router.navigate(['/profile']);
+  }
+  toggleDropdown(): void {
+    this.showDropdown = !this.showDropdown;
+  }
+}
