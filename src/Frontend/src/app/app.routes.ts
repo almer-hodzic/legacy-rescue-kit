7408d3ea5fc95cb0b1dae6ff40b35bb7bc6cd19e
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-06-14 17:09:31 +0200

    feat(routing): add protected routes

--- src/Frontend/src/app/app.routes.ts
+++ src/Frontend/src/app/app.routes.ts
@@ -3,11 +3,16 @@ import {TaskListComponent} from './tasks/task-list/task-list.component';
 import {TaskFormComponent} from './tasks/task-form/task-form.component';
 import {LoginComponent} from './_auth/auth/login/login.component';
 import {RegisterComponent} from './_auth/auth/register/register.component';
+import {UserProfileComponent} from './_profile/user-profile/user-profile.component';
+import {roleGuard} from './_auth/auth/guards/role.guard';
+import {AdminDashboardComponent} from './_admin/admin/admin-dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/tasks', pathMatch: 'full' },
   { path: 'tasks', component: TaskListComponent },
   { path: 'tasks/new', component: TaskFormComponent },
   { path: 'auth/login', component: LoginComponent },
-  { path: 'auth/register', component: RegisterComponent }
+  { path: 'auth/register', component: RegisterComponent },
+  { path: 'profile', component: UserProfileComponent },
+  { path: 'admin', component: AdminDashboardComponent, canActivate: [roleGuard('Admin')] }
 ];
