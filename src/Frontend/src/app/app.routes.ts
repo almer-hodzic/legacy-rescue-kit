cc06e2c853e6589b8dced4cb2845a7d59ab5c2c7
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-06-23 12:02:35 +0200

    feat(routing): set default path to start dashboard component

--- src/Frontend/src/app/app.routes.ts
+++ src/Frontend/src/app/app.routes.ts
@@ -8,9 +8,10 @@ import {roleGuard} from './_auth/auth/guards/role.guard';
 import {AdminDashboardComponent} from './_admin/admin/components/dashboard/admin-dashboard.component';
 import {AdminUsersComponent} from './_admin/admin/components/admin-users/admin-users.component';
 import {AdminTasksComponent} from './_admin/admin/components/tasks/admin-tasks.component';
+import {StartDashboardComponent} from './_dashboard/dashboard/start-dashboard.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: '', component: StartDashboardComponent },
   { path: 'tasks', component: TaskListComponent },
   { path: 'tasks/new', component: TaskFormComponent },
   { path: 'auth/login', component: LoginComponent },
