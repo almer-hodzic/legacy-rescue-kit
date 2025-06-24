714e3fe0a15995dad18d23751fa4f91482358253
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-06-24 09:26:43 +0200

    feat(routes): add authGuard and update route definitions to combine auth and role-based access control

--- src/Frontend/src/app/app.routes.ts
+++ src/Frontend/src/app/app.routes.ts
@@ -9,15 +9,16 @@ import {AdminDashboardComponent} from './_admin/admin/components/dashboard/admin
 import {AdminUsersComponent} from './_admin/admin/components/admin-users/admin-users.component';
 import {AdminTasksComponent} from './_admin/admin/components/tasks/admin-tasks.component';
 import {StartDashboardComponent} from './_dashboard/dashboard/start-dashboard.component';
+import { authGuard } from './_auth/auth/guards/auth.guard'
 
 export const routes: Routes = [
   { path: '', component: StartDashboardComponent },
-  { path: 'tasks', component: TaskListComponent },
-  { path: 'tasks/new', component: TaskFormComponent },
+  { path: 'tasks', component: TaskListComponent, canActivate: [authGuard] },
+  { path: 'tasks/new', component: TaskFormComponent, canActivate: [authGuard] },
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
-  { path: 'profile', component: UserProfileComponent },
-  { path: 'admin', component: AdminDashboardComponent, canActivate: [roleGuard('Admin')] },
-  { path: 'admin/users', component: AdminUsersComponent, canActivate: [roleGuard('Admin')] },
-  { path: 'admin/tasks', component: AdminTasksComponent, canActivate: [roleGuard('Admin')] }
+  { path: 'profile', component: UserProfileComponent , canActivate: [authGuard]},
+  { path: 'admin', component: AdminDashboardComponent, canActivate: [authGuard, roleGuard('Admin')] },
+  { path: 'admin/users', component: AdminUsersComponent, canActivate: [authGuard, roleGuard('Admin')] },
+  { path: 'admin/tasks', component: AdminTasksComponent, canActivate: [authGuard, roleGuard('Admin')] }
 ];
