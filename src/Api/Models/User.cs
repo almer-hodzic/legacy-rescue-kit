07256a89439c5148477ee7146efef5499ee95f5a
almer-hodzic <137727738+almer-hodzic@users.noreply.github.com>
2025-06-26 08:47:52 +0200

    feat(models): add RefreshToken and RefreshTokenExpiresAt fields in User

--- src/Api/Models/User.cs
+++ src/Api/Models/User.cs
@@ -5,4 +5,7 @@ namespace Api.Models;
 public class User : IdentityUser
 {
     public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiresAt { get; set; }
 }
